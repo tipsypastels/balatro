1c89293a4f2a0de18353082e7453cd907ddd26c9
tipsypastels <dakotasankey@gmail.com>
2025-07-12 09:41:37 -0700

    add rank predicates

--- src/rank.rs
+++ src/rank.rs
@@ -1,6 +1,7 @@
+use balatro_macros::Variants;
 use enum_assoc::Assoc;
 
-#[derive(Assoc, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Assoc, Variants, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 #[func(pub const fn score(self) -> u64)]
 pub enum Rank {
     #[assoc(score = 2)]
@@ -29,4 +30,51 @@ pub enum Rank {
     King,
     #[assoc(score = 11)]
     Ace,
+}
+
+impl Rank {
+    pub const fn is_face(self) -> bool {
+        matches!(self, Self::Jack | Self::Queen | Self::King)
+    }
+
+    pub const fn is_odd(self) -> bool {
+        !self.score().is_multiple_of(2)
+    }
+
+    pub const fn is_even(self) -> bool {
+        !self.is_face() && !self.is_odd()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use Rank::*;
+
+    #[test]
+    fn face() {
+        for rank in Rank::variants() {
+            assert_eq!(rank.is_face(), matches!(rank, Jack | Queen | King));
+        }
+    }
+
+    #[test]
+    fn odd() {
+        for rank in Rank::variants() {
+            assert_eq!(
+                rank.is_odd(),
+                matches!(rank, Three | Five | Seven | Nine | Ace),
+            );
+        }
+    }
+
+    #[test]
+    fn even() {
+        for rank in Rank::variants() {
+            assert_eq!(
+                rank.is_even(),
+                matches!(rank, Two | Four | Six | Eight | Ten),
+            )
+        }
+    }
 }
